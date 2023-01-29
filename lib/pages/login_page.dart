978782b335c5afcc8f0e7af0c9fe86d0984dd4c9
Ranit003 <ranitsharma121@gmail.com>
2023-01-29 11:02:41 +0530

    done

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
+//import 'package:flutter/scheduler.dart';
 
 import '../utils/routes.dart';
 
 
- class LoginPage extends StatelessWidget {
+ class LoginPage extends StatefulWidget {
    const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+   String name=' ';
+   bool changeButton=false;
    @override
    Widget build(BuildContext context) {
      return   Material(
@@ -16,19 +23,19 @@ import '../utils/routes.dart';
                fit:BoxFit.cover,
 
              ),
-             SizedBox(
+             const SizedBox(
                height:20.0,
              ),
 
              Text(
-               'Welcome',
-                   style:TextStyle(
+               'Welcome $name',
+                   style:const TextStyle(
                      fontSize:24.0,
                      fontWeight: FontWeight.bold,
 
              ),
              ),
-               SizedBox(
+               const SizedBox(
                  height:20.0,
                ),
                Padding(
@@ -36,14 +43,20 @@ import '../utils/routes.dart';
                  child: Column(
                    children: [
                      TextFormField(
-                       decoration: InputDecoration(
+                       decoration:const  InputDecoration(
                          hintText:'Enter username',
                          labelText:"Username",
                        ),
+                       onChanged: (value){
+                         name=value;
+                         setState(() {
+
+                         });
+                       },
                      ),
                      TextFormField(
                      obscureText: true,
-                       decoration: InputDecoration(
+                       decoration: const InputDecoration(
                          hintText:'Enter password',
                          labelText:"password",
                        ),
@@ -51,22 +64,52 @@ import '../utils/routes.dart';
                    ],
                  ),
                ),
-               SizedBox(
+               const SizedBox(
                  height: 20.0,
                ),
-               ElevatedButton(onPressed:( ){
-                 Navigator.pushNamed(context, MyRoutes.homeRoute);
-               },
-                   child: Text('login'),
-                 style: TextButton.styleFrom(minimumSize: Size(150, 100)),
+               // ElevatedButton(onPressed:( ){
+               //   Navigator.pushNamed(context, MyRoutes.homeRoute);
+               // },
+               //   style: TextButton.styleFrom(minimumSize:const  Size(150, 100)),
+               //     child:const Text('login'),
+               //
+               // ),
+               InkWell(
+                 onTap: () async {
+                  setState(() {
+                    changeButton=true;
+                  });
+                  await Future.delayed(Duration(seconds: 1));
+                  Navigator.pushNamed(context,MyRoutes.homeRoute);
+                   },
+                 child: AnimatedContainer(
+                   duration:const Duration(seconds:1) ,
+                   width:changeButton ? 50 : 150,
+                   height: 50,
+                   //color: Colors.deepPurple,
+                   alignment: Alignment.center,
+                   decoration: BoxDecoration(
+                     color: Colors.deepPurple,
+                      //  shape: changeButton
+                      //      ?BoxShape.circle
+                      //  :BoxShape.rectangle,
+                       borderRadius: BorderRadius.circular(changeButton ? 20 : 8.0)
+                   ),
+                   child: changeButton?Icon(Icons.done,color: Colors.white,): Text(
+                     'login',
+                     style: TextStyle(
+                       color:Colors.white,fontWeight: FontWeight.bold,fontSize:22
 
-               ),
+                     ),
+                   ),
+                 ),
+               )
               ],
 
-             
+
 
            ),
          )
      );
    }
- }
+}
