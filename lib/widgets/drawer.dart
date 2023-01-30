c3b84fc2c5f7e251ee08a44f231d2b908273f3ad
Ranit003 <ranitsharma121@gmail.com>
2023-01-30 12:33:46 +0530

    done

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl='https://i.pinimg.com/550x/31/23/2f/31232fe4b51b47763282524f008d9081.jpg';
+    return Drawer(
+      child: Container(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                accountName: Text('Ranit Tatrial'),
+                accountEmail: Text('ranitdqs@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(imageUrl),
+                ),
+
+              ),
+            ),
+            ListTile(
+            leading:Icon(CupertinoIcons.home,
+            color: Colors.green,
+            ),
+            title: Text('Home',style:TextStyle(
+              color: Colors.green,
+                ) ,
+              ),
+            ),
+             ListTile(
+              leading:Icon(CupertinoIcons.profile_circled,
+                color: Colors.green,
+              ),
+              title: Text('Profile',style:TextStyle(
+                color: Colors.green,
+              ) ,
+              ),
+            ),
+            ListTile(
+              leading:Icon(CupertinoIcons.mail,
+                color: Colors.green,
+              ),
+              title: Text('Email me',style:TextStyle(
+                color: Colors.green,
+              ) ,
+              ),
+            ),
+          ],
+        ),
+
+      ),
+    );
+  }
+}
