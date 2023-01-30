3f97f9d394ee47338620966d4bc9c875a36d5478
Ranit003 <ranitsharma121@gmail.com>
2023-01-30 16:56:27 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:day4/pages/login_page.dart';
+import 'package:day4/utils/routes.dart';
+import 'package:day4/widget/themes.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +15,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
-      home: Material(
-          child:Center(
-            child:Container(
-            child:const Text ("Flutter Demo Home Page"),
-    ),
-    ),
-      ),
+      themeMode: ThemeMode.light,
+      theme: MyTheme.LightTheme(context),
+      darkTheme:  MyTheme.DarkTheme(context),
+
+      debugShowCheckedModeBanner: false,
+      initialRoute:MyRoutes.homeRoute,
+      routes: {
+        '/':(context)=> const LoginPage(),
+        MyRoutes.homeRoute:(context)=>const HomePage(),
+        MyRoutes.loginRoute:(context)=>const LoginPage(),
+      },
+      //home:HomePage(),
     );
   }
 }
