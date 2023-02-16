900e7adc5a0f96f72ac4ca3e906955886b142926
Ranit003 <ranitsharma121@gmail.com>
2023-02-16 17:08:20 +0530

    done

---
+++ lib/models/catalog.dart
@@ -0,0 +1,16 @@
+import 'dart:core';
+
+
+class Item{
+final String id;
+final String name;
+final String desc;
+final String price;
+final String color;
+final String image;
+
+
+  Item(this.id, this.name, this.desc, this.price, this.color, this.image);
+
+}
+final products=[Item("fuijcd", "hewhj","gdyhekj", "76", "#00ac51","" )];
